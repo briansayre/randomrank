d55d190e16c4db6925110d147438e089c279d048
Brian Sayre <bsayre@epic.com>
2023-04-11 12:58:07 -0500

    updates

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var score = 0;
 var bigNumber;
 
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 function gameOver() {
   alert("game over\nscore: " + score.toString());
   location.reload();
@@ -15,6 +19,7 @@ function bigNumberToStr() {
 
 function assignNumber() {
   bigNumber = Math.floor(Math.random() * 999 + 1);
+  var buttons = document.getElementById("buttons").querySelectorAll("button");
   for (var i = 0; i < buttons.length; i++) {
     if (buttons[i].innerHTML != "&nbsp;") {
       var numInBut = parseInt(buttons[i].innerHTML);
@@ -24,7 +29,31 @@ function assignNumber() {
       }
     }
   }
+  var minIdx = -1;
+  var maxIdx = 20;
+  for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].innerHTML != "&nbsp;") {
+      var numInBut = parseInt(buttons[i].innerHTML);
+      if (bigNumber < numInBut) minIdx = i;
+      if (bigNumber > numInBut) {
+        maxIdx = i;
+        break;
+      }
+    }
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    if (i > minIdx && i < maxIdx) {
+      buttons[i].style.backgroundColor = "#bad4bb";
+    } else {
+      buttons[i].style.backgroundColor = "";
+    }
+  }
   document.getElementById("number").innerHTML = bigNumberToStr();
+  if (maxIdx - minIdx <= 1) {
+    sleep(500).then(() => {
+      gameOver();
+    });
+  }
 }
 
 function handleButton(num) {
