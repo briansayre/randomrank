c4bad6befb0a2ed58299e5c6f859fb8f76f85663
Brian Sayre <bsayre@epic.com>
2023-04-11 09:18:17 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,45 +1,57 @@
 var score = 0;
-var bigNumber
-
-function randomIntFromInterval(min, max) { // min and max included 
-	return Math.floor(Math.random() * (max - min + 1) + min)
-}
+var bigNumber;
 
 function gameOver() {
-	alert("GAME OVER\nScore: " + score.toString());
-	location.reload()
+  alert("game over\nscore: " + score.toString());
+  location.reload();
+}
+
+function bigNumberToStr() {
+	var bigNumberStr = bigNumber.toString()
+	if (bigNumberStr.length == 2) bigNumberStr = "0" + bigNumberStr
+	else if (bigNumberStr.length == 1) bigNumberStr = "00" + bigNumberStr
+	return bigNumberStr
 }
 
 function assignNumber() {
-	bigNumber = randomIntFromInterval(0, 1000);
-	document.getElementById("number").innerHTML = bigNumber;
+  bigNumber = Math.floor(Math.random() * (999) + 1);
+  for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].innerHTML != "&nbsp;") {
+      var numInBut = parseInt(buttons[i].innerHTML);
+      if (numInBut === bigNumber) {
+        assignNumber();
+        return;
+      }
+    }
+  }
+  document.getElementById("number").innerHTML = bigNumberToStr();
 }
 
 function handleButton(num) {
-	if (document.getElementById("b"+num.toString()).innerHTML != '') {
-		gameOver();
-		return
-	}
-	var buttons = document.getElementById("buttons").querySelectorAll("button");
-	for (var i = num+1; i < buttons.length; i++) {
-		if (buttons[i].innerHTML != '') {
-			var numInBut = parseInt(buttons[i].innerHTML)
-			if (numInBut > bigNumber) {
-				gameOver();
-				return
-			}
-		}
-	}
-	for (var i = num-1; i >= 0; i--) {
-		if (buttons[i].innerHTML != '') {
-			var numInBut = parseInt(buttons[i].innerHTML)
-			if (numInBut < bigNumber) {
-				gameOver();
-				return
-			}
-		}
-	}
-	document.getElementById("b"+num.toString()).innerHTML = bigNumber;
-	score++;
-	assignNumber();
+  if (document.getElementById("b" + num.toString()).innerHTML != "&nbsp;") {
+    gameOver();
+    return;
+  }
+  var buttons = document.getElementById("buttons").querySelectorAll("button");
+  for (var i = num + 1; i < buttons.length; i++) {
+    if (buttons[i].innerHTML != "&nbsp;") {
+      var numInBut = parseInt(buttons[i].innerHTML);
+      if (numInBut > bigNumber) {
+        gameOver();
+        return;
+      }
+    }
+  }
+  for (var i = num - 1; i >= 0; i--) {
+    if (buttons[i].innerHTML != "&nbsp;") {
+      var numInBut = parseInt(buttons[i].innerHTML);
+      if (numInBut < bigNumber) {
+        gameOver();
+        return;
+      }
+    }
+  }
+  document.getElementById("b" + num.toString()).innerHTML = bigNumberToStr();
+  score++;
+  assignNumber();
 }
